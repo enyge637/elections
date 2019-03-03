e570a45f5d3ca69360b6ae1f43b733f59dbc3614
enyge637 <enyge637@gmail.com>
2019-03-04 01:05:26 +0900

    hi

---
+++ mysite/elections/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from . import views
+
+app_name = 'elections'
+urlpatterns = [
+    url(r'^$', views.index, name = 'home'),
+    url(r'^$', views.index),
+    url(r'^areas/(?P<area>[가-힣]+)/$', views.areas),
+    url(r'^areas/(?P<area>[가-힣]+)/results$', views.results),
+    url(r'^polls/(?P<poll_id>\d+)/$', views.polls),
+]
